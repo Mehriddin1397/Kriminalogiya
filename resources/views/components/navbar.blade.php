78575f4fce9fac859d2fe5792ddfce4abb1a023c
Mehriddin1397 <soyibovmehriddin@gmail.com>
2025-05-06 16:30:33 +0500

    ilmiy darajani ochdim

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -192,7 +192,7 @@
             <div class="nav-item dropdown">
                 <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">{{__('lan.ilm_sal')}}</a>
                 <div class="dropdown-menu fade-up m-0">
-                    <a href="#" class="dropdown-item">{{__('lan.ilm_dara')}}</a>  {{--{{route('show',['category_id'=>3,'id'=>4])}} --}}
+                    <a href="{{route('show',['category_id'=>27,'id'=>1])}}" class="dropdown-item">{{__('lan.ilm_dara')}}</a>  {{--{{route('show',['category_id'=>3,'id'=>4])}} --}}
                     <a href="#" class="dropdown-item">{{__('lan.kur_amal')}}</a> {{--{{route('categoryId',37)}} --}}
                     <a href="#" class="dropdown-item">{{__('lan.maqolalar')}}</a> {{--{{route('categoryId',23)}} --}}
                 </div>
