88143b458bacd68271f7fc2eaa2249c7daa70ce1
Mehriddin1397 <soyibovmehriddin@gmail.com>
2025-07-01 11:04:53 +0500

    ip quyildi

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->alias([
+            'ip.restrict' => App\Http\Middleware\RestrictLoginByIP::class,
+        ]);
         $middleware->web(append: [
             \App\Http\Middleware\LocaleMiddleware::class,
         ]);
